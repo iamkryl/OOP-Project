8e3f2d4f99b1b1d4cab8273ff3ef3853fb6ceec0
iamkryl <karylleamad1@gmail.com>
2024-10-23 00:11:25 +0800

    SiITYou.java

---
+++ 4th Draft/SiITYou.java
@@ -0,0 +1,15 @@
+public class SiITYou extends Kingdom{
+
+    public SiITYou() {
+        super("Si IT You?");
+     }
+     @Override
+     public void enter() {
+        System.out.println("\t\t\t   Welcome to " + name + ", the land of coding challenges!");
+    }
+
+    @Override
+    public void startQuest(){
+        
+    }
+}
