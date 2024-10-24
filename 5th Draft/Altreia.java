fb7367fd26903d83f68996dc85849cba5267293e
iamkryl <karylleamad1@gmail.com>
2024-10-24 11:54:24 +0800

    Altreia.java

---
+++ 5th Draft/Altreia.java
@@ -0,0 +1,35 @@
+public class Altreia extends Hero {
+    public Altreia() {
+        super("Altreia", 230, 250); 
+    }
+    
+    @Override
+    public int attack(int choice) {
+        int damage;
+        int manaCost;
+
+        switch (choice) {
+            case 1:
+                damage = 20;
+                manaCost = 15;
+                break;
+            case 2:
+                damage = 35;
+                manaCost = 50;
+                break;
+            case 3:
+                damage = 45;
+                manaCost = 70;
+                break;
+            default:
+                return -1; // Invalid choice
+        }
+
+        if (getMana() >= manaCost) {
+            setMana(getMana() - manaCost); // Deduct mana
+            return damage; // Return the damage dealt
+        } else {
+            return -2; // Not enough mana
+        }
+    }
+}
